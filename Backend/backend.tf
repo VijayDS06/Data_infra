4a7e92e4d46cfe140fcd4f0eca06056975dbe0f9
VijayDS06 <vijayslm06@gmail.com>
2022-06-25 19:27:35 +0530

    tf state update

--- Backend/backend.tf
+++ Backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "vj-data-infra-terraform-sf"
-    key = "main"
+    key = "terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "Terraform-Jenkins-table"
   }
